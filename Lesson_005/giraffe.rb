b589f5c9c77dd66ae2cc9c37b9280eca250a4b14
Plex <wematall@gmail.com>
2022-09-07 10:34:14 +0300

    simple drawing

---
+++ Lesson_005/giraffe.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+
+# difference between print and puts
+# print no new line
+# puts make a new line encoding at the encoding
+
+puts "Plex"
+puts " is cool"
+print "Giraffe Academy is cool"
+puts ""
+puts "And some simple drawing"
+puts "/___|"
+puts "   /|"
+puts "  / |"
+puts " /  |"
+puts "/   |"
