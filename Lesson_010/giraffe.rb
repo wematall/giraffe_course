5dd0476d450c8c5bb6e1aa92726e870dfaef1120
plex <plex@onion.localdomain>
2022-09-08 11:25:44 +0300

    getting input from user, basic input

---
+++ Lesson_010/giraffe.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+
+# Getting user input
+
+puts "Enter you name: "
+name = gets.chomp()
+
+puts ("Hello, #{name}, you are cool!")
+puts "Enter how old are you: "
+age = gets.chomp()
+puts ("#{name} you are #{age} years old.")
