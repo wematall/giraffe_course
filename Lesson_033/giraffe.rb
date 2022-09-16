dd952f2d4a73587c7dc2e6e68c61e9ab1ae8b3e4
plex <plex@onion.localdomain>
2022-09-16 14:50:03 +0300

    inheritance

---
+++ Lesson_033/giraffe.rb
@@ -0,0 +1,36 @@
+# Inheritance
+
+class Chef
+  def make_chiken
+    puts "The chef makes chicken"
+  end
+
+  def make_salad
+    puts "The chef makes salad"
+  end
+
+  def make_special_dish
+    puts "The chef makes bbq ribs"
+  end
+end
+
+
+class ItalianChef < Chef
+  def make_special_dish
+    puts "The chef makes eggplant parm"
+  end
+
+  def make_pasta
+    puts "The chef makes pasta"
+  end
+end
+
+
+chef = Chef.new()
+chef.make_chiken
+chef.make_special_dish
+
+italian_chef = ItalianChef.new()
+italian_chef.make_salad
+italian_chef.make_special_dish
+italian_chef.make_pasta
